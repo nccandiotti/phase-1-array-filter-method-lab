6c897332ce9684a1f0518ed5366f8a71722181d8
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-12 19:59:30 -0500

    commit tree 04240815ef29bae42a7c4daf02a8f7853311cd92

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+"strict";
+
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+
+const findMatching = function (arrayOfDrivers, name) {
+  let matching = arrayOfDrivers.filter(
+    (driver) =>
+      driver === name ||
+      driver === name.toUpperCase() ||
+      driver === name.toLowerCase()
+  );
+  if (matching) {
+    return matching;
+  } else return [];
+};
+const fuzzyMatch = function (arrayOfDrivers, driver) {
+  let firstTwo = arrayOfDrivers.filter(
+    (name) => name.substring(0, 2) === driver.substring(0, 2)
+  );
+  if (firstTwo) return firstTwo;
+};
+const matchName = function (arrayOfDrivers, string) {
+  let match = arrayOfDrivers.filter((name) => name.name === string);
+  if (match) return match;
+};
